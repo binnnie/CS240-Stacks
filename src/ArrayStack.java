c17282eb06d834eeb51f39ba01760d24af3902cc
Fyrecean <fyrecean@gmail.com>
2018-04-10 13:05:08 -0700

    fixed LOL

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,20 +1,60 @@
-public class ArrayStack implements DStack{
+import java.util.EmptyStackException;
 
-   private int size;
-   private double[] data;
-   
-   public ArrayStack(){
-      size = 0;
-      data = new double[10];
-   }
-   
-   public boolean isEmpty(){
-      return size == 0;
-   }
-   
-   public void push(double d){
-      if(data.length < size){
-         data[size] = d;
-      }
-   }
+public class ArrayStack implements DStack{
+    private int size;
+    private double[] data;
+
+    public ArrayStack(){
+        size = 0;
+        data = new double[10];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void push(double d) {
+        if (size == data.length){
+            extend();
+        }
+        data[size] = d;
+        size++;
+    }
+
+    @Override
+    public double pop() {
+        if (isEmpty()){
+            throw new EmptyStackException();
+        } else {
+            size--;
+            return data[size];
+        }
+    }
+
+    @Override
+    public double peek() {
+        if (isEmpty()){
+            throw new EmptyStackException();
+        } else {
+            return data[size - 1];
+        }
+    }
+
+    private void extend(){
+        double[] hold = new double[data.length*2];
+        for (int i = 0; i <= size-1; i++){
+            hold[i] = data[i];
+        }
+        data = hold;
+    }
+
+    public String toString(){
+        String out = "[";
+        for(int i = 0; i < size-1; i++){
+            out += data[i]+", ";
+        }
+        return out+data[size-1]+"]";
+    }
 }
