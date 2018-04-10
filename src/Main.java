5d8c6b7b966d94fdc2dce807dc848711a3a98c59
Fyrecean <fyrecean@gmail.com>
2018-04-10 12:49:23 -0700

    finished

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        ListStack test = new ListStack();
+        for (int i = 0; i <= 11; i++){
+            test.push(i+.1);
+        }
+        System.out.println(test.toString());
+        System.out.println(test.pop());
+        System.out.println(test.peek());
+    }
+}
