19e8aaf3338da912179593595904f4a15a044701
unknown <BDIXON@starfleet.local>
2018-04-17 12:05:14 -0700

    added method comments for the ArrayStack.java file

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,20 +1,22 @@
-// test
 import java.util.EmptyStackException;
 
 public class ArrayStack implements DStack{
     private int size;
     private double[] data;
 
+    //Constructor: Creates an empty stack. Initializes the size field and the data field.
     public ArrayStack(){
         size = 0;
         data = new double[10];
     }
 
+    //Checks if the stack is empty. Returns true if it is empty and false if it isn't.
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    //Accepts a double parameter d and adds that value to the top of the stack.
     @Override
     public void push(double d) {
         if (size == data.length){
@@ -24,6 +26,7 @@ public class ArrayStack implements DStack{
         size++;
     }
 
+    //Removes the top value from the stack and returns that value. Throws an EmptyStackException if the stack is empty.
     @Override
     public double pop() {
         if (isEmpty()){
@@ -34,6 +37,7 @@ public class ArrayStack implements DStack{
         }
     }
 
+    //Returns the top value of the stack without removing it. Throws an EmptyStackException if the stack is empty.
     @Override
     public double peek() {
         if (isEmpty()){
@@ -43,6 +47,7 @@ public class ArrayStack implements DStack{
         }
     }
 
+    //Private method to extend the length of the data array field by a factor of 2.
     private void extend(){
         double[] hold = new double[data.length*2];
         for (int i = 0; i <= size-1; i++){
@@ -51,6 +56,7 @@ public class ArrayStack implements DStack{
         data = hold;
     }
 
+    //Returns a string to represent the data in the stack.
     public String toString(){
         String out = "[";
         for(int i = 0; i < size-1; i++){
