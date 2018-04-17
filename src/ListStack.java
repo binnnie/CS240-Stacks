3e14ea146b30d30a50e870ce9d4f5306370f34a7
unknown <BDIXON@starfleet.local>
2018-04-17 11:54:30 -0700

    added comments for methods in ListStack.java

--- src/ListStack.java
+++ src/ListStack.java
@@ -4,15 +4,18 @@ public class ListStack implements DStack{
 
     private Node last;
 
+    //Constructor: creates an empty stack. Initialize the last field as null.
     public ListStack(){
         last = null;
     }
 
+    //Checks if the stack is empty; returns true if it is, false if it is not.
     @Override
     public boolean isEmpty() {
         return last == null;
     }
 
+    //Accepts a double parameter and adds it to the top of the stack.
     @Override
     public void push(double d) {
         if (isEmpty()){
@@ -23,6 +26,7 @@ public class ListStack implements DStack{
         }
     }
 
+    //Removes the top value from the stack and returns that value. Throws EmptyStackException if the stack is empty.
     @Override
     public double pop() {
         if (isEmpty()) {
@@ -35,6 +39,7 @@ public class ListStack implements DStack{
         }
     }
 
+    //Returns the top value of the stack without removing it. Throws EmptyStackException if the stack is empty.
     @Override
     public double peek() {
         if (isEmpty()) {
@@ -44,16 +49,19 @@ public class ListStack implements DStack{
         }
     }
 
+    //Returns a string representing the data contained in the stack.
     public String toString(){
         String out = last.data+"]";
         return stringIt(last.prev, out);
     }
 
+    //Private helper method to recurse through the stack. Accepts Node parameter current which is the current node
+    //and a String out which will be returned later.
     private String stringIt(Node current, String out){
         if (current.prev == null){
             return "["+out;
         } else {
-            return stringIt(current.prev, current.data+", "+out);
+            return stringIt(current.prev, current.data + ", " + out);
         }
     }
 
@@ -62,12 +70,15 @@ public class ListStack implements DStack{
         private Node prev;
         private double data;
 
+        //Constructor for the inner class Node. Accepts Node parameter p and double parameter d and initializes the
+        //fields prev, next, and data.
         private Node(Node p, double d){
             prev = p;
             data = d;
             next = null;
         }
 
+        //Returns a string representing the Node's data.
         public String toString(){
             return data+"";
         }
